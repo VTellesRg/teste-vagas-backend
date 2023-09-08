da8b36cda49ad1839e0559848c43fed182e4102d
VTellesRg <vini.m.telles@gmail.com>
2023-09-08 18:04:06 -0300

    add: validation and handle functions

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from "express";
-import { Product } from "../models/Product";
+import { Product, ProductInstance } from "../models/Product";
 import fs from "fs";
+import csvParser from "csv-parser";
 
-export const upload = (req: Request, res: Response) => {
-	console.log(req.file);
-	if(req.file === undefined) return res.status(400).json({});
-	const stream = fs.createReadStream(req.file?.path);
-
-	return res.json({});
+export const upload = async (req: Request, res: Response) => {
+	const { file } = req;
+	const productsList: (ProductInstance | null)[] = [];
+	const resp = await handleProducts(file as Express.Multer.File);
+	const result = await getProducts(resp);
+	// console.log(result);
+	
+	const validProducts = validateProducts(result);
+		// console.log(produto);
+		
+		
+	return res.json(validProducts);
 }
-
 export const update = (req: Request, res: Response) => {
 	return res.json({});
 }
@@ -23,4 +29,100 @@ export const createProduct = async (req: Request, res: Response) => {
 		sales_price,
 	});
 	res.json(newProfuct);
+}
+
+interface fileUpload {
+	product_code: number;
+	new_price: number;
+}
+
+function handleProducts(file: Express.Multer.File): Promise<fileUpload[]> {
+	return new Promise((resolve, reject) => {
+
+		const stream = fs.createReadStream(file.path);
+
+		const parseFile = csvParser();
+
+		stream.pipe(parseFile);
+
+		const products: fileUpload[] = [];
+
+		parseFile.on('data', async (line) => {
+			const { product_code, new_price } = line;
+			products.push({
+				product_code,
+				new_price,
+			})
+		})
+			.on('end', () => {
+				resolve(products);
+			}).on('error', (err) => {
+				reject(err);
+			});
+	});
+}
+
+async function getProducts (array: any[]) {
+	const productList = [];
+
+	for(let i= 0; i < array.length; i++) {
+		const produto = await Product.findOne({
+			where: {
+				code: array[i].product_code,
+			}
+		});
+		productList.push({...produto?.dataValues, 'new_price': array[i].new_price});
+	};
+	return productList;
+}
+
+function validateProducts(productList: any[]) {
+	const validProducts = [];
+	
+	for(let i = 0; i < productList.length; i++) {
+	if(productList[i].code == undefined || productList[i].code == null) {
+		validProducts.push({
+			'code': 'Código inválido',
+			'name': '-',
+			'cost_price': '-',
+			'sales_price': '-',
+			'valid_from': 'Código inválido'
+		});
+	
+	} else {
+		if(productList[i].new_price > productList[i].cost_price) {
+			if(productList[i].new_price >= productList[i].sales_price*0.9 && productList[i].new_price <= productList[i].sales_price*1.1) {
+				validProducts.push({
+					'code': productList[i].code,
+					'name': productList[i].name,
+					'cost_price': productList[i].cost_price,
+					'sales_price': productList[i].sales_price,
+					'valid_from': 'OK'
+				});
+				
+			} else {
+				validProducts.push({
+					'code': productList[i].code,
+					'name': productList[i].name,
+					'cost_price': productList[i].cost_price,
+					'sales_price': productList[i].sales_price,
+					'valid_from': 'Preço inválido, fora da margem de 10%'
+				}); 
+				break;
+			}
+		} else {
+			validProducts.push({
+				'code': productList[i].code,
+				'name': productList[i].name,
+				'cost_price': productList[i].cost_price,
+				'sales_price': productList[i].sales_price,
+				'valid_from': 'Preço inválido, menor que o custo'
+			});
+		}
+	}
+	
+	}
+	
+	return  validProducts;
+
 }
