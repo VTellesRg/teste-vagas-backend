6158ba8998fd377bc3eb1269a1caaad9bc692792
VTellesRg <vini.m.telles@gmail.com>
2023-09-11 00:31:43 -0300

    fix: update functions

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,85 +1,73 @@
 import { Request, Response } from "express";
-import { Product, ProductInstance } from "../models/Product";
+import { Product } from "../models/Product";
+import { Packs } from "../models/Packs";
 import fs from "fs";
 import csvParser from "csv-parser";
-import { Packs, PacksInstance } from "../models/Packs";
 
 export const upload = async (req: Request, res: Response) => {
 	const { file } = req;
+
 	if (file === undefined) {
 		return res.status(400).json({
-			 error: 'Arquivo não enviado' });
-	}
-	if (file.mimetype !== 'text/csv') {
-		return res.status(400).json({
-			error: 'Arquivo enviado não é do formato CSV' });
+			message: 'File not found',
+		});
 	}
 
-	// const productsList: (ProductInstance | null)[] = [];
-	const resp: any[]= await handleFile(file as Express.Multer.File);
-	let validProducts: any[] = [];
-	let validPacks: any[] = [];
-	if (resp[0].length >= 4) {
-		/*
-			if(product_code[].lenght >= 4){
-				é um pack, então:
-				1. pegar o id do pack
-				2. verifica se existe o pack (se sim, pega o qty, se nao, retorna erro 1 da funçao de validacao)
-				3. dividir o new_price (nessa situação new_price deve ser para um pack) pelo qty
-				4. salvar como new_price/sales_price do produto
-				5. enviar o item para a validação
-				6. retornar o "erro equivalente" ou "ok" da alteração
-				
-			}
-		*/
-		const packsResult = await getPacks(resp);
-		console.log(packsResult);
-		// validPacks = validateProducts(packsResult);
-		// return res.json(validPacks);
+	if (file.mimetype !== 'text/csv') {
+		return res.status(400).json({
+			message: 'file csv not found',
+		});
 	}
-	else {
-	const productResult = await getProducts(resp);
-	// const packsList: (PacksInstance | null)[] = [];
-	// console.log(productResult);
-	validProducts = validateProducts(productResult);
-	
-	return res.json(validProducts);
-	}
-		// console.log(produto);	
+
+	const resp = await handleFile(file as Express.Multer.File);
+
+	const formatedResp = await getProducts(resp);
+
+	return res.json(formatedResp);
 }
-//função para enviar a atualização ao BD
-export const update = (req: Request, res: Response) => {
-	const jsonData = req.body;
-	const key = Object.keys(jsonData)[0];
-	try {
-		let obj = JSON.parse(key);
-		console.log(obj);
-	} catch (error) {
-		;
+
+export const update = async (req: Request, res: Response) => {
+	const obj = Object.keys(req.body)[0];
+	const json = await JSON.parse(obj);
+
+	for (let i = 0; i < json.length; i++) {
+		const product = await Product.update({
+			sales_price: json[i].new_price,
+		}, {
+			where: {
+				code: json[i].code,
+			}
+		});
+
+		let percent = (json[i].new_price / json[i].sales_price);
+
+		const pack = await Packs.findAll({
+			where: {
+				pack_id: json[i].code,
+			}
+		});
+		for (let j = 0; j < pack.length; j++) {
+			const product = await Product.findOne({
+				where: {
+					code: pack[j].dataValues.product_id,
+				}
+			});
+
+			await product?.update({
+				sales_price: product.dataValues.sales_price * percent,
+				
+			},
+				{
+					where: {
+						code: product?.dataValues.code,
+					}
+				}
+			);
+			product?.save();
+		}
 	}
-	// const obj = JSON.parse(json as any);
-	const data: any[] = [];
-	// console.log(obj);
 	
-	
-	for (let i = 0; i < data.length; i++) {
-		console.log(data[i]);
-		
-		// if(data[i]) {
-		// 	console.log('entrou');
-			
-		// 	Product.update({
-		// 		sales_price: data[i].new_price,
-		// 	}, {
-		// 		where: {
-		// 			code: data[i].code,
-		// 		}
-		// 	});
-		// 	const percent = ((data[i].new_price/data[i].sales_price));
-		// 	console.log(percent);
-		// }
-	}
-	return res.json({});
+	return res.json(obj);
 }
 
 export const createProduct = async (req: Request, res: Response) => {
@@ -100,6 +88,7 @@ interface fileUpload {
 
 function handleFile(file: Express.Multer.File): Promise<fileUpload[]> {
 	return new Promise((resolve, reject) => {
+
 		const stream = fs.createReadStream(file.path);
 
 		const parseFile = csvParser();
@@ -123,87 +112,63 @@ function handleFile(file: Express.Multer.File): Promise<fileUpload[]> {
 	});
 }
 
-async function getProducts (data: any[]) {
-	let productList: any[] = [];
+async function getProducts(data: fileUpload[]) {
+	let productsList: any[] = []
 
-	for(let i= 0; i < data.length; i++) {
-		const produto = await Product.findOne({
+	for (let i = 0; i < data.length; i++) {
+		const product = await Product.findOne({
 			where: {
 				code: data[i].product_code,
 			}
 		});
-		productList.push({...produto?.dataValues,
-			 'new_price': data[i].new_price});
-	};
-	return productList;
-}
+		const new_price = parseFloat(data[i].new_price.toString());
+		const cost_price = parseFloat(product?.dataValues.cost_price.toString());
+		const sales_price = parseFloat(product?.dataValues.sales_price.toString());
 
-function validateProducts(productList: any[]) {
-	const validProducts = [];
-	
-	for(let i = 0; i < productList.length; i++) {
-	if(productList[i].code == undefined || productList[i].code == null) {
-		validProducts.push({
-			'code': 'Código inválido',
-			'name': '-',
-			'cost_price': '-',
-			'sales_price': '-',
-			'new_price': '-',
-			'valid_from': 'Código inválido'
-		});
-	
-	} else {
-		if(productList[i].new_price > productList[i].cost_price) {
-			if(productList[i].new_price >= productList[i].sales_price*0.9 && productList[i].new_price <= productList[i].sales_price*1.1) {
-				validProducts.push({
-					'code': productList[i].code,
-					'name': productList[i].name,
-					'cost_price': productList[i].cost_price,
-					'sales_price': productList[i].sales_price,
-					'new_price': productList[i].new_price,
-					'valid_from': 'OK'
-				});
-				
-			} else {
-				validProducts.push({
-					'code': productList[i].code,
-					'name': productList[i].name,
-					'cost_price': productList[i].cost_price,
-					'sales_price': productList[i].sales_price,
-					'new_price': productList[i].new_price,
-					'valid_from': 'Preço inválido, fora da margem de 10%'
-				}); 
-				break;
-			}
-		} else {
-			validProducts.push({
-				'code': productList[i].code,
-				'name': productList[i].name,
-				'cost_price': productList[i].cost_price,
-				'sales_price': productList[i].sales_price,
-				'new_price': productList[i].new_price,
-				'valid_from': 'Preço inválido, menor que o custo'
+		if (!product?.dataValues) {
+			productsList.push({
+				code: data[i].product_code,
+				name: '-',
+				cost_price: '-',
+				sales_price: '-',
+				new_price: data[i].new_price,
+				valid_from: 'Produto não encontrado',
 			});
-		}
-	}
-	
-	}
-	
-	return  validProducts;
-
-}
-
-async function getPacks (array: any[]) {
-
-	const packList = [];
-
-	for(let i= 0; i < array.length; i++) {
-		const pack = await Packs.findOne({
-			where: {
-				id: array[i].id,
+		} else {
+			if (product && new_price < cost_price) {
+				productsList.push({
+					code: product.dataValues.code,
+					name: product.dataValues.name,
+					cost_price: product.dataValues.cost_price,
+					sales_price: product.dataValues.sales_price,
+					new_price: data[i].new_price,
+					valid_from: 'preco de venda menor que o de custo',
+				});
+			} else {
+				if (product && new_price > (sales_price * 1.1) || product && new_price < (sales_price * 0.9)) {
+					productsList.push({
+						code: product.dataValues.code,
+						name: product.dataValues.name,
+						cost_price: product.dataValues.cost_price,
+						sales_price: product.dataValues.sales_price,
+						new_price: data[i].new_price,
+						valid_from: 'preco de venda fora da margem de 10%',
+					});
+				} else {
+					if (product?.dataValues) {
+						productsList.push({
+							code: product.dataValues.code,
+							name: product.dataValues.name,
+							cost_price: product.dataValues.cost_price,
+							sales_price: product.dataValues.sales_price,
+							new_price: data[i].new_price,
+							valid_from: 'OK',
+						});
+					}
+				}
 			}
-		});
-		packList.push({...pack?.dataValues});
+		}
 	};
-	return packList;
-} 
+
+	return productsList;
+}
